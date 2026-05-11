f8a23d7e4881af7649d4dc293cc8c151b42bd157
Karan pawar <karanpawar1383@gmail.com>
2026-05-11 09:21:13 +0530

    Binary to decimal completed

---
+++ binary-to-decimal/BinaryToDecimal.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class BinaryToDecimal{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter a binary number: ");
+        int binary = obj.nextInt();
+
+        int decimal = 0;
+        int base = 1;
+
+        while(binary > 0){
+
+            int digit = binary % 10;
+            decimal = decimal + digit * base;
+            base = base * 2;
+            binary = binary / 10;
+        }
+        System.out.println("Decimal number = " + decimal);
+    }
+}
