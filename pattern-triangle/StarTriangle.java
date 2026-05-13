6ad3fb269dc5645934394ab4f90edeb21cbdfadf
Karan pawar <karanpawar1383@gmail.com>
2026-05-13 16:02:13 +0530

    Reverse added

--- pattern-triangle/StarTriangle.java
+++ pattern-triangle/StarTriangle.java
@@ -9,5 +9,19 @@ class StarTriangle{
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println("Reverse Triangle: ");
+        System.out.println();
+
+        for(int i = 5; i >= 1; i--){
+
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+            
     }
 }
+
