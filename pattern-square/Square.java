876647026e54492ceae21a0569ce022337bfa307
Karan pawar <karanpawar1383@gmail.com>
2026-05-14 14:46:31 +0530

    Square pattern done

---
+++ pattern-square/Square.java
@@ -0,0 +1,13 @@
+
+class Square{
+    public static void main(String args[]){
+        int num = 4;
+
+        for(int i = 1; i <= num; i++){
+            for(int j = 1; j <= num; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
